69c081ed3bb7602ff4ac5fb7e497f6fade000a04
Jack <furegin@yandex.ru>
2021-03-23 12:39:52 +0500

    fix

--- src/response/ActionInterceptor/index.js
+++ src/response/ActionInterceptor/index.js
@@ -13,7 +13,7 @@ const ActionInterceptor = (options) =>
       : { type: 'blob' },
     )
 
-    if (action){
+    if (action) {
       action.run(options, response)
       throw new axios.Cancel({
         action,
@@ -23,4 +23,31 @@ const ActionInterceptor = (options) =>
     return response
   }
 
+const defaultInterceptorConfig = () => ({
+  actionAttributeName: 'status',
+})
+
+const ActionInterceptorBuild = (interceptorConfig = defaultInterceptorConfig()) => (options) =>
+  /**
+   * @param {ResponseWrapper} response
+   * @return {ResponseWrapper}
+   */
+    (response) => {
+    const action = buildAction(!response.isBinary()
+      ? response.extra(interceptorConfig.actionAttributeName)
+      : { type: 'blob' },
+    )
+
+    if (action) {
+      action.run(options, response)
+      throw new axios.Cancel(action)
+    }
+
+    return response
+  }
+
 export default ActionInterceptor
+
+export {
+  ActionInterceptorBuild,
+}
