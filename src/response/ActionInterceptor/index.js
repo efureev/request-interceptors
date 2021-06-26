c91bddbf21d42dbdb299476b6ba732d9fb2489ed
Jack <furegin@yandex.ru>
2021-06-26 12:32:19 +0500

    fix

--- src/response/ActionInterceptor/index.js
+++ src/response/ActionInterceptor/index.js
@@ -13,28 +13,40 @@ const errHandler = (interceptorConfig) => (error) => {
 
     if (action) {
       action.run(config, error.response)
-      throw new axios.Cancel()
+      if (configLayer.extra.onlyOneAction) {
+        const err = new axios.Cancel()
+        err.response = response
+
+        throw err
+      }
+      // throw new axios.Cancel()
     }
   }
 
   return Promise.reject(error)
 }
 
-const ActionInterceptorBuild = (interceptorConfig = defaultInterceptorConfig()) => (options) =>
+const ActionInterceptorBuild = (interceptorConfig = defaultInterceptorConfig()) => (configLayer) =>
   [
     /**
      * @param {ResponseWrapper} response
      * @return {ResponseWrapper}
      */
       (response) => {
+
       const action = buildAction(!response.isBinary()
         ? response.extra(interceptorConfig.actionAttributeName)
         : { type: 'blob' },
       )
 
       if (action) {
-        action.run(options, response)
-        throw new axios.Cancel()
+        action.run(configLayer, response)
+        if (configLayer.extra.onlyOneAction) {
+          const err = new axios.Cancel()
+          err.response = response
+
+          throw err
+        }
       }
 
       return response
