a276a6569283a41e8d047dd816cc9128d618eef3
Jack <furegin@yandex.ru>
2021-08-09 22:41:01 +0500

    feat: add

--- src/response/ActionInterceptor/actions/ActionsManager.js
+++ src/response/ActionInterceptor/actions/ActionsManager.js
@@ -3,10 +3,28 @@ class ActionsManager {
     this.list = {}
   }
 
+  /**
+   * @param {String} type
+   * @param {String} action
+   */
   add(type, action) {
     this.list[type] = action
   }
 
+  /**
+   * @param {Array} typeList
+   * @param {String} action
+   */
+  addList(typeList, action) {
+    typeList.forEach((type) => {
+      this.add(type, action)
+    })
+  }
+
+  /**
+   * @param {String} type
+   * @return {*}
+   */
   get(type) {
     return this.list[type] ?? null
   }
