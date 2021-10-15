d69548c32e33a04001bb95c428591b3d0dd08c3d
Jack <furegin@yandex.ru>
2021-10-15 12:46:03 +0500

    feat: add target to Redirect

--- src/response/ActionInterceptor/actions/RedirectAction.js
+++ src/response/ActionInterceptor/actions/RedirectAction.js
@@ -4,14 +4,32 @@ export default class RedirectAction extends BaseAction {
   constructor(data) {
     super(data)
     this.url = data.url
+    this.target = data.target
+
+    this.normalizeTarget()
   }
 
   run(axiosConfig) {
     if (/^http(s)?:\/\//.test(this.url)) {
-      window.location = this.url
+
+      this.openUrl(this.url)
+
       return
     }
 
-    window.location = new URL(window.location.origin + this.url)
+    const url = new URL(window.location.origin + this.url)
+
+    this.openUrl(url)
+  }
+
+  openUrl(url) {
+    window.open(url, this.target) || window.location.replace(url)
+  }
+
+  normalizeTarget() {
+    if (this.target === '_blank' || this.target === '_self') {
+      return
+    }
+    this.target = '_self'
   }
 }
