f182acc06655532f5c2f22136a5cc7384c0ef002
Jack <furegin@yandex.ru>
2022-08-07 15:18:17 +0500

    fix

--- src/request/AuthInterceptor.ts
+++ src/request/AuthInterceptor.ts
@@ -3,16 +3,17 @@ import type { ExtraProperties, InterceptorSuccessParam, LayerConfig } from '@feu
 import type { AxiosRequestConfig, AxiosRequestHeaders } from 'axios'
 
 const AuthInterceptor =
-  (options: LayerConfig, extra: ExtraProperties): InterceptorSuccessParam<AxiosRequestConfig> =>
-  (config: AxiosRequestConfig): AxiosRequestConfig => {
-    const auth = options.getExtra('auth')
-    if (auth) {
-      ;(<AxiosRequestHeaders>config.headers).Authorization = isFunction(auth) ? auth(config) : config.auth
-    } else {
-      config.headers && delete config.headers.Authorization
-    }
+  (layerConfig: LayerConfig, extra: ExtraProperties): InterceptorSuccessParam<AxiosRequestConfig> =>
+    (config: AxiosRequestConfig): AxiosRequestConfig => {
+      const auth = layerConfig.getExtra('auth')
+      if (auth) {
+        ;(<AxiosRequestHeaders>config.headers).Authorization = isFunction(auth) ? auth(config) : config.auth
+      }
+      else {
+        config.headers && delete config.headers.Authorization
+      }
 
-    return config
-  }
+      return config
+    }
 
 export default AuthInterceptor
